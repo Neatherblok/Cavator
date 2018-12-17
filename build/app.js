7fed0977a47a89609bd83e6f0d01669dfa43b14b
Neatherblok <42801543+Neatherblok@users.noreply.github.com>
2018-12-17 15:57:31 +0100

    new cursor feather

--- build/app.js
+++ build/app.js
@@ -69,7 +69,7 @@ class Game {
                             this.currentGameScreenNumber = 2;
                             this.canvasElement.style.backgroundImage = "url(./assets/images/backgrounds/tableBackgroundConcept.jpg)";
                             this.canvasElement.style.backgroundSize = "100% 100%";
-                            this.canvasElement.style.cursor = "url(./assets/images/FeatherCursor.png), auto";
+                            this.canvasElement.style.cursor = "url(./assets/images/FeatherCursor2.png), auto";
                             this.Gamescreen.regenerateHole(i);
                         }
                     }
@@ -110,7 +110,7 @@ function init() {
     const cavator = new Game();
     cavator.draw();
     window.addEventListener("click", cavator.nextScreen);
-    cavator.canvasElement.style.cursor = "url(./assets/images/featherCursor.png), auto";
+    cavator.canvasElement.style.cursor = "url(./assets/images/FeatherCursor2.png), auto";
 }
 class MouseListener {
     constructor() {
