452e77ae9273205318fcfdd7c813e61a9e5c55f7
Jarno Korstanje <kors0023@hz.nl>
2018-12-18 17:12:05 +0100

    background music plays in loop

--- build/app.js
+++ build/app.js
@@ -124,6 +124,7 @@ class Game {
         this._gameHelper = new GameHelper();
         let audioLink = `./assets/sounds/music/dutch_street_organ.wav`;
         let backgroundMusic = new Audio(audioLink);
+        backgroundMusic.loop = true;
         backgroundMusic.play();
     }
 }
