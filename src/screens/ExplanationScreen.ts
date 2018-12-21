32a5488db56f95cbfcdded80e6a2fb0eb3770295
Jarno Korstanje <kors0023@hz.nl>
2018-12-21 11:06:23 +0100

    grotschildering image

--- src/screens/ExplanationScreen.ts
+++ src/screens/ExplanationScreen.ts
@@ -2,14 +2,16 @@ class ExplanationScreen{
 
     protected readonly canvasElement: HTMLCanvasElement;
     protected readonly _canvas: CanvasHelper;
+    protected readonly explanation: string;
     
     public constructor(){
         this.canvasElement = <HTMLCanvasElement>document.getElementById('canvas');
         this._canvas = new CanvasHelper(this.canvasElement);
+        this.explanation = "Het doel om het spel is om zoveel mogelijk voorwerpen op te graven ";
     }
 
     public draw = () => {
-        this._canvas.writeTextToCanvas("Uitleg",20,this._canvas.getCenter().X,this._canvas.getCenter().Y);
+        this._canvas.writeTextToCanvas(this.explanation,20,this._canvas.getCenter().X,this._canvas.getCenter().Y);
         this._canvas.writeButtonToCanvas("Terug naar titelscherm", undefined, this._canvas.getCenter().Y + 200);
 
     }
